14a685061058156e41e729b0f7ca0b74e106431c
Terry <terry@terrytilley.com>
2017-02-22 16:46:22 +0000

    add json file to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
+import pageContent from '../data/page-content.json';
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = pageContent;
+  }
   render() {
     return(
       <div>
